03fbd8e59a5d70f7b8ea90e64153912aefab7e91
Wonjong Park <wonjong2@gmail.com>
2022-07-17 13:57:54 -0700

    Updates Frontend component

--- src/components/Frontend.js
+++ src/components/Frontend.js
@@ -1,10 +1,21 @@
 import React from 'react';
 
 export default function Frontend() {
+    const frontEndTech = [
+        'HTML',
+        'CSS',
+        'JavaScript',
+        'jQuery',
+        'React',
+        'Bootstrap',
+    ];
 
     return (
         <div>
-
+            <p>Front-end Proficiencies</p>
+            <ul>
+                {frontEndTech.map((item, index) => <li key={index}>{item}</li>)}
+            </ul>
         </div>
     )
 }
