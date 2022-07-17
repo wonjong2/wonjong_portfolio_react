266bd924e9a55893ac8c137482d95144dca54cf6
Wonjong Park <wonjong2@gmail.com>
2022-07-17 14:07:13 -0700

    Updates Backend component

--- src/components/Backend.js
+++ src/components/Backend.js
@@ -1,10 +1,21 @@
 import React from 'react';
 
 export default function Backend() {
+    const backEndTech = [
+        'APIs',
+        'Node',
+        'Express',
+        'MySQL, Sequelize',
+        'MongoDB, Mongoose',
+        'REST',
+    ];
 
     return (
         <div>
-
+            <p>Back-end Proficiencies</p>
+            <ul>
+                {backEndTech.map((item, index) => <li key={index}>{item}</li>)}
+            </ul>
         </div>
     )
 }
