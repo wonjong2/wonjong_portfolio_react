384872454eab69a28523a6a01505404ac080f2e8
Wonjong Park <wonjong2@gmail.com>
2022-07-17 13:51:58 -0700

    Adds components for Resume section

--- src/components/Resume.js
+++ src/components/Resume.js
@@ -1,5 +1,19 @@
 import React from 'react';
+import Frontend from './Frontend';
+import Backend from './Backend';
+import resume from '../documents/Wonjong_Park_Resume.pdf'
 
 export default function Resume() {
-    return <p>Resume</p>
+
+    return (
+        <div>
+            <h3>Resume</h3>
+            <div>
+                <p>Download my <a href={resume} target="_blank" rel="noreferrer">Resume</a>
+                </p>
+            </div>
+            <Frontend />
+            <Backend />
+        </div>
+    )
 }
