e12b3e772c47be81a8ee0d5be7b395162b4a0cdd
Wonjong Park <wonjong2@gmail.com>
2022-07-18 12:18:12 -0700

    Updates Footer component

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,5 +1,22 @@
 import React from 'react';
 
+const styles = {
+    alignIcons: {
+        justifyContent: 'center',
+    },
+    icon: {
+        fontSize: '2rem',
+        color: 'white',
+    }
+}
 export default function Footer() {
-    return <p>Footer</p>;
+    return (
+        <footer className='container-fluid d-flex pt-5' style={styles.alignIcons}>
+            <div style={styles.icon}>
+                <a className='me-3' href='https://github.com/wonjong2' target='_blank' rel='noreferrer'><i className='fa-brands fa-github'></i></a>
+                <a className='me-3' href='https://www.linkedin.com/in/won-jong-park-a7a45146/' target='_blank' rel='noreferrer'><i className="fa-brands fa-linkedin"></i></a>
+                <a className='me-3' href='https://stackoverflow.com/users/18087428/wonjong-park2' target='_blank' rel='noreferrer'><i className="fa-brands fa-stack-overflow"></i></a>
+            </div>
+        </footer>
+    )
 };
